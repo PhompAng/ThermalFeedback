285415607c01a3061c4a317868efac7b89d89fe6
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-01-17 16:15:19 +0700

    retrieve user

--- app/src/main/java/com/example/phompang/thermalfeedback/UsersActivity.java
+++ app/src/main/java/com/example/phompang/thermalfeedback/UsersActivity.java
@@ -10,6 +10,11 @@ import android.view.MenuItem;
 
 import com.example.phompang.thermalfeedback.adapter.UserAdapter;
 import com.example.phompang.thermalfeedback.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,8 @@ public class UsersActivity extends AppCompatActivity {
 
     private UserAdapter adapter;
     private List<User> users;
+    private DatabaseReference reference;
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,28 +45,47 @@ public class UsersActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Users Management");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        makeList();
+        reference = FirebaseDatabase.getInstance().getReference();
+        userRef = reference.child("users");
+
+        users = new ArrayList<>();
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         adapter = new UserAdapter(this, users);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-
     }
 
-    private void makeList() {
-        users = new ArrayList<>();
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
-        users.add(new User());
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userRef.addValueEventListener(listener);
     }
+
+    private ValueEventListener listener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            users.clear();
+            for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                users.add(snapshot.getValue(User.class));
+            }
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
 
     @OnClick(R.id.add)
     public void add() {
         startActivity(new Intent(this, AddUserActivity.class));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        userRef.removeEventListener(listener);
     }
 
     @Override
--- app/src/main/java/com/example/phompang/thermalfeedback/adapter/UserAdapter.java
+++ app/src/main/java/com/example/phompang/thermalfeedback/adapter/UserAdapter.java
@@ -5,11 +5,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.phompang.thermalfeedback.R;
 import com.example.phompang.thermalfeedback.model.User;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by phompang on 12/29/2016 AD.
@@ -33,7 +39,9 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        User u = users.get(position);
+        holder.name.setText(u.getName() + " " + u.getSurname());
+        holder.phone.setText(u.getUid());
     }
 
     @Override
@@ -42,8 +50,17 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.img)
+        ImageView img;
+        @BindView(R.id.name)
+        TextView name;
+        @BindView(R.id.phone)
+        TextView phone;
+        @BindView(R.id.delete)
+        ImageButton delete;
         ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
