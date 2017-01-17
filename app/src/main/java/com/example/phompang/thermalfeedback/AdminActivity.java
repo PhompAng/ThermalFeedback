6c031da20b4dc8e2198e37ceb1c1f024972c07dc
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-01-17 12:43:21 +0700

    admin login

--- app/src/main/java/com/example/phompang/thermalfeedback/AdminActivity.java
+++ app/src/main/java/com/example/phompang/thermalfeedback/AdminActivity.java
@@ -1,10 +1,20 @@
 package com.example.phompang.thermalfeedback;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +23,12 @@ import butterknife.OnClick;
 public class AdminActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.username)
+    TextInputEditText username;
+    @BindView(R.id.password)
+    TextInputEditText password;
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +39,52 @@ public class AdminActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Admin Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 
     @OnClick(R.id.submit)
+    public void validate() {
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(password.getText())) {
+            password.setError("Should not empty");
+            cancel = true;
+            focusView = password;
+        }
+        if (TextUtils.isEmpty(username.getText())) {
+            username.setError("Should not empty");
+            cancel = true;
+            focusView = username;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            login();
+        }
+    }
+
     public void login() {
-        startActivity(new Intent(AdminActivity.this, SettingActivity.class));
-        finish();
+        databaseReference.child("admin").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String adminUser = dataSnapshot.child("username").getValue(String.class);
+                String adminPass = dataSnapshot.child("password").getValue(String.class);
+                if (adminUser.equals(username.getText().toString()) && adminPass.equals(password.getText().toString())) {
+                    startActivity(new Intent(AdminActivity.this, SettingActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong credential!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
